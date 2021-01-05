a21c39a9b50e6ecc1c685fb8d01789e95eabbf7f
Endcyclone <49358328+Endcyclone@users.noreply.github.com>
2021-01-05 14:22:55 +0900

    Create auto_video_speed_x2.0.user.js

---
+++ auto_video_speed_x2.0.user.js
@@ -0,0 +1,26 @@
+﻿// ==UserScript==
+// @name         Auto Video Speed x2.0
+// @version      1.0
+// @description  NEC研修のビデオが自動的に倍速から始まります
+// @author       Kosuke Tomita
+// @match        https://nec.etudes.jp/scorm-customize/content.html?key=*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    let $ = window.jQuery;
+
+    let myfunc = () => {
+        $('iframe').load(() => {
+            let $iframe = $('iframe', $('iframe').contents());
+            $iframe.load(() => {
+                let $video = $('video', $iframe.contents());
+                $video.prop('playbackRate', 2);
+            });
+        });
+    }
+
+    myfunc();
+    $('#next-button').on('click', myfunc);
+
+})();
